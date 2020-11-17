32231659f6d5c476aa7a08620843dc81bca2afe1
Mskrost <74321599+Mskrost@users.noreply.github.com>
2020-11-17 16:11:03 +0100

    js update

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$('#first').click(() => {
+    console.log('Yeah, you clicked me');
+});
+
+$('#second').click(() => {
+    $('#first').text('Change');
+});
+
+$('#third').click(() => {
+    $('button').css('background', 'yellow');
+});
